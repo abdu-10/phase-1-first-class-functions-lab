d1d7d5b2f5bbdc08c051a8da6062925852d1bae8
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-11-26 17:54:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers = function block(){
+    return (['Antonia', 'Nuru'])}
+    const  returnLastTwoDrivers = function block(){
+        return (['Amari', 'Mo']);
+    
+    }
+    const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+    function createFareMultiplier(number){
+        return function(fare){
+          return fare*number;
+        };
+    }
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+    
+    const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+        return driversToReturn(arrayOfDrivers);
+      };
